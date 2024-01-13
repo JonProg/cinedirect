317e727f1241c7352391d32e7d8b6a362633e864
jon_prog <jonashenri.aquino@gmail.com>
2024-01-13 04:12:40 -0300

    add:fuction modDirect

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.get('https://topflix.sh/filmes/assistir-online-asdgni435/ ')
-print(response.url)
+#response = requests.get('https://topflix.sh/filmes/assistir-online-asdgni435/ ')
+#print(response.url) para saber se a rota gerada foi para 404
+
+name_movie = 'Tempos Modernos'
+
+def modeDirect(movie:str):
+    return movie.lower().replace(' ','-');
+
+print(modeDirect(name_movie))
