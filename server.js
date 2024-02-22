e8fed58e9d7eca9497443781e060dfe6923a0af8
jon_prog <jonashenri.aquino@gmail.com>
2024-02-22 12:19:24 -0300

    update:server.js

--- server.js
+++ server.js
@@ -1,26 +1,7 @@
-const express = require('express');
-const routes = require('./routes');
-const app = express();
-const path = require('path');
+import app from './app';
 
-app.use(express.urlencoded({extended:true}));
-app.use(express.json());
-app.use(express.static(path.resolve(__dirname,'public')));
-
-/*
-A pasta public é automaticamente colocada no diretorio de pastas do servidor
-fazendo com que pareça que as views estão dentro da pasta public facilitando
-na hora de colocar os caminhos dos aquivos estáticos
-*/
-
-app.set('views', path.resolve(__dirname,'src','views'));
-app.set('view engine','ejs'); //Serve para adicinar a logica do js no html
-
-app.use(routes);
- 
-
-app.listen(3000,()=>{
-    console.log('Acess http://localhost:3000');
-    console.log('Server online on port 3000...');
+app.listen(3000, () => {
+  console.log('Acess http://localhost:3000');
+  console.log('Server online on port 3000...');
 });
     
