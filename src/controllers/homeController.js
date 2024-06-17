ba8097a3e57c21f726015ab7580af532e3a4a53e
jon_prog <jonashenri.aquino@gmail.com>
2024-06-17 10:26:19 -0300

    modfiding homeController

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -2,6 +2,9 @@ import dotenv from 'dotenv'
 import axios from 'axios'
 
 dotenv.config()
+axios.defaults.headers.common['Authorization'] = `Bearer ${process.env.API_KEY}`;
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
 class HomeController{
     async index(req,res){
         res.render('index');
@@ -11,7 +14,7 @@ class HomeController{
         const inputValue = req.body.inputName;
         process.env.API_KEY
         try {
-            const response = await axios.get('https://api.themoviedb.org/3/authentication');
+            const response = await axios.get('https://api.themoviedb.org/3/authentication')
             console.log(response.data);
         } catch (error) {
             console.error('Erro ao fazer a requisição GET:', error);
