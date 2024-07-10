dd9ee6972613399f2dba228a4bcb1d053b506b5b
jon_prog <jonashenri.aquino@gmail.com>
2024-07-10 19:29:00 -0300

    testing code in homecontroller

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -5,18 +5,19 @@ dotenv.config()
 
 const serchURL = process.env.API_SEARCH;
 const apiKey = process.env.API_KEY;
+let movies = [];
 
 class HomeController{
     async index(req,res){
-        res.render('index');
+        res.render('index', {movies});
     };
 
     async submit(req,res){
         const inputValue = req.body.inputName;
         try {
             const response = await axios.get(`${serchURL}${apiKey}&query=${inputValue}&include_adult=false&language=pt-BR`)
-            var movies = response.data.results.slice(0,10)
-            res.render('index', movies)
+            movies = response.data.results.slice(0,10)
+            res.render('index',{movies})
         } catch (error) {
             console.error('Erro ao fazer a requisição GET:', error);
         }
