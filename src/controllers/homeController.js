af9ebe65e682a50ce9a302cba6beb9d987ae88da
jon_prog <jonashenri.aquino@gmail.com>
2024-07-22 06:01:51 -0300

    modified code

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -15,7 +15,8 @@ class HomeController{
         const inputValue = req.body.inputName;
         try {
             const response = await axios.get(`${serchURL}${apiKey}&query=${inputValue}&include_adult=false&language=pt-BR`)
-            console.log(response.data.results.slice(0,10))
+            movies = response.data.results.slice(0,10);
+            res.render('index', {movies})
         } catch (error) {
             console.error('Erro ao fazer a requisição GET:', error);
         }
