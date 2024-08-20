6cadb9af99f2c1bc3f9c8899a05fc4a81a3bd350
jon_prog <jonashenri.aquino@gmail.com>
2024-08-20 07:57:18 -0300

    logic for request websites

--- data.js
+++ data.js
@@ -1,4 +1,6 @@
-const movies = [
+import axios from 'axios'
+
+const links = [
     {
         name: "Mega Filmes HD",
         link: "https://www.megafilmeshds.net/",
@@ -7,7 +9,8 @@ const movies = [
     {
         name: "Mega Filmes Gratis HD",
         link: "https://megafilmesgratishd.com/filme/assistir-",
-        division_url:"-"
+        division_url:"-",
+        endUrl:"-dublado-online-hdd"
     },
     {
         name: "Topflix",
@@ -21,3 +24,35 @@ const movies = [
         endUrl:"-online"
     },
 ]
+
+var movieLinks = []
+
+async function linkMovies(filme){
+    for (const website of links) {
+        let movieLink = `${website.link}${filme.split(' ').join(website.division_url)}`;
+        if (website.endUrl) {
+            movieLink += website.endUrl
+        };
+        console.log(movieLink)
+        movieLinks.push(movieLink)
+    }
+}
+
+async function validLink(links){
+    var responses = []
+    for (const link of links) {
+        const response = await axios.get(link)
+        responses.push(response.status)
+    }
+    console.log(responses)
+}
+
+process.stdout.write('Nome do filme: ');
+process.stdin.on('data', async function(data) {
+    const filme = data.toString().trim().toLowerCase(); 
+    await linkMovies(filme)
+    await validLink(movieLinks)
+    process.exit();
+});
+
+
