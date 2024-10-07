f8881d268c48457da26c4d3ea3519dfa711480da
JonProg <jonashenri.aquino@gmail.com>
2024-10-07 19:17:44 -0300

    feat: mofied test

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
-import app from './app';
+import express from 'express';
+import homeRoutes from './src/routes/homeRoutes';
+import movieRoutes from './src/routes/movieRoutes';
+import { resolve } from 'path';
 import dotenv from 'dotenv';
 
-dotenv.config();
+class App {
+  constructor() {
+    dotenv.config(); // Carrega variáveis de ambiente do .env
+    this.app = express();
+    this.middlewares();
+    this.routes();
+    this.setters();
+    this.listen(); // Inicia o servidor aqui
+  }
 
-const port = process.env.PORT || 3000;
+  middlewares() {
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.json());
+    this.app.use(express.static(resolve(__dirname, 'src', 'assets')));
+  }
 
-app.listen(port, () => {
-  console.log(`Acess http://localhost: ${port}`);
-  console.log(`Server online on port ${port}...`);
-});
-    
+  routes() {
+    this.app.use('/', homeRoutes);
+    this.app.use('/movie', movieRoutes);
+  }
+
+  setters() {
+    this.app.set('views', resolve(__dirname, 'src', 'views'));
+    this.app.set('view engine', 'ejs'); // Serve para adicionar a lógica do js no html
+  }
+
+  listen() {
+    const port = process.env.PORT || 3000; // Define a porta a ser utilizada
+    this.app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+  }
+}
+
+export default new App().app; // Exporta a instância do app
