923df1d9870b90f7865598c93a2aa36f9ab1d773
JonProg <jonashenri.aquino@gmail.com>
2024-10-07 17:34:33 -0300

    feat: mofied deploy

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 import app from './app';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Acess http://localhost: ${port}`);
+  console.log(`Acess http://localhost:${port}`);
   console.log(`Server online on port ${port}...`);
 });
-    
