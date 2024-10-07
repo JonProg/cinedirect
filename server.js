af57e895694c44d432f87eb1a8832093adbf36ce
JonProg <jonashenri.aquino@gmail.com>
2024-10-07 18:14:28 -0300

    feat: mofied test

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 import app from './app';
 import dotenv from 'dotenv'
+import express from 'express';
 
 dotenv.config()
 
 const port = process.env.PORT || 3000;
+const app = express()
+
+app.use("/", function(req,res){
+  res.send("Server é o caramba")
+})
 
 app.listen(port, () => {
   console.log(`Acess http://localhost:${port}`);
