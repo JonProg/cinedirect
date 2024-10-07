718629973478a1fed632ef5155e2f8b7b65c2c28
JonProg <jonashenri.aquino@gmail.com>
2024-10-07 19:14:12 -0300

    feat: mofied test

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-import express from 'express';
+import app from './app';
 import dotenv from 'dotenv';
 
-const app = express()
-
 dotenv.config();
-
-app.use("/", function(req,res){
-  return res.json("seu merda")
-})
 
 const port = process.env.PORT || 3000;
 
