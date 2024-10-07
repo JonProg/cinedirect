a55741c3249e991acea4755035ac99e3fffcf507
JonProg <jonashenri.aquino@gmail.com>
2024-10-07 20:02:45 -0300

    feat: mofied test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
-import homeRoutes from 'src/routes/homeRoutes';
-import movieRoutes from 'src/routes/movieRoutes';
+import homeRoutes from 'src/routes/homeRoutes.js';
+import movieRoutes from 'src/routes/movieRoutes.js';
 import express from 'express';
 
 dotenv.config();
