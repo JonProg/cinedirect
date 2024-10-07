9eb875594e4c58fca2d8957ea59cf99171e52946
JonProg <jonashenri.aquino@gmail.com>
2024-10-07 20:17:30 -0300

    feat: mofied test

--- src/routes/homeRoutes.js
+++ src/routes/homeRoutes.js
@@ -1,11 +1,9 @@
 import { Router } from 'express';
-
+import homeController from '../controllers/homeController';
 
 const router = new Router();
 
-router.get('/', (req, res) => {
-    res.json('Lista de filmes');
-  })
-
+router.get('/', homeController.index);
+router.get('/search', homeController.search);
 
 export default router;
